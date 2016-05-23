ca8e892908e01329eeedc75a76e24c4396dcc02a
IsVivian <384120230@qq.com>
2016-05-23 15:19:12 +0800

    CustomTabBarController_for_Swift

--- CustomTabBar/AppDelegate.swift
+++ CustomTabBar/AppDelegate.swift
@@ -41,11 +41,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         settingVC.title = "设置"
         
         let customTabBar = CustomTabBarController()
+        //设置标签栏的背景图片
         customTabBar.tabBarView.image = UIImage(named: "mask_navbar")
+        //设置选中图片的滑动
         customTabBar.selectedImg = UIImage(named: "home_bottom_tab_arrow@2x")
+        //设置默认选中的视图
         customTabBar.selectCount = 0
+        //加入视图控制器的数组
         customTabBar.viewControllers = [homeVC.navi, messageVC.navi, UIViewController(), personVC.navi, settingVC.navi]
         
+        //设置为根视图控制器
         self.window?.rootViewController = customTabBar
         
         return true
